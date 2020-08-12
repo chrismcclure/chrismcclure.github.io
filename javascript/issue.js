9537d344c5f1d6ec98bef059cffe1382e265e430
Chris McClure <chris.m.mcclure@gmail.com>
2020-08-12 05:35:43 -0500

    renamed file in js

--- javascript/issue.js
+++ javascript/issue.js
@@ -9,7 +9,7 @@ var selectedIssue;
 var openDate;
 
 ///Parse the files
-Papa.parse(csvFilePath, {
+Papa.parse(csvFilePath + "?_=" + (new Date).getTime(), {
     header: true,
     download: true,
     dynamicTyping: true,
@@ -49,7 +49,8 @@ function setThePage(){
         console.log('Selected issue null or undefined')
         return
     }
-
+   
+    console.log("here is the issue from memory");
     console.log(selectedIssue);
     var title = document.getElementById('Issue-Name');
     title.innerText = selectedIssue.title;
@@ -58,11 +59,13 @@ function setThePage(){
     description.innerText = selectedIssue.description;
 
     if(selectedIssue.id === 4){
-        Papa.parse(issueFile4 +"?_="+ (new Date).getTime(), {
+        console.log('issues 4');
+        Papa.parse(issueFile4, {
             header: true,
             download: true,
             dynamicTyping: true,
             complete: function (results) {        
+                console.log("parsed the file for isssue 4");
             issuesHistory = results.data;               
                 FillInHistory(issuesHistory);
                 var creatorSpan = document.getElementById('creator');
@@ -80,12 +83,13 @@ function setThePage(){
 
     if(selectedIssue.id === 1){
         console.log('issues 1');
-        Papa.parse(issueFile1 +"?_="+ (new Date).getTime(), {
+        Papa.parse(issueFile1, {
             header: true,
             download: true,
             dynamicTyping: true,
             complete: function (results) {        
-            issuesHistory = results.data;               
+            issuesHistory = results.data;  
+            console.log("parsed the file for isssue 1");             
                 FillInHistory(issuesHistory);
                 var creatorSpan = document.getElementById('creator');
                 creatorSpan.innerText =  selectedIssue.inputer+  " - " + openDate;
